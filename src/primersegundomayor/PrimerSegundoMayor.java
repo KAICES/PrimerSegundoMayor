e516f3c38cc2ac7b8c2f1fade4caa47d0cec1c59
cesar.ramirez <cesar.ramirez@Tecnologia3.solucionescorp.local>
2017-05-08 16:35:27 -0500

    falta que el usuario ingrese datos al array.

--- src/primersegundomayor/PrimerSegundoMayor.java
+++ src/primersegundomayor/PrimerSegundoMayor.java
@@ -3,13 +3,10 @@ package primersegundomayor;
 import java.io.PrintStream;
 import java.util.Arrays;
 import javax.swing.JOptionPane;
-
 /**
  *
  * @author Cesar Ramirez ID 378938
  */
-
-
 public class PrimerSegundoMayor {
 
     public static int Maximo(int[] a){
@@ -21,42 +18,42 @@ public class PrimerSegundoMayor {
         
         int medio = (inf + sup ) / 2 ;
         int maxIzq = Maximo(a, inf, medio);
-        int maxDer = Maximo (a, medio + 1, sup);
-        
-        if (maxIzq > maxDer ){
-            
-            return maxIzq;
-        }else{                    
-            return maxDer;   
-            }
+        int maxDer = Maximo (a, medio + 1, sup);        
+        return Math.max(maxIzq, maxDer);
+
         }    
-    
-    /////***** funcion para elevar a la 2 los numeros del array ***********
-    
-        public static int[] multiplicar (int [] enteros){
-            
-        int [] r = new int [enteros.length ];
+  
+    public static int Minimo(int[] a){
+        return Minimo(a, 0 , a.length - 1);
+    }
+    static int Minimo(int[] a, int inf, int sup ){
         
- 
-        for ( int i = 0; i < enteros.length ; i++){
-                int mul = enteros[i] * enteros[i];
-                r[i] = mul ;   
-                       
-            }
-           return r ;
-        }
-    
-    
-    
-    
+        if ( inf == sup ) return a [inf];
+        
+        int medio = (inf + sup ) / 2 ;
+        int maxIzq = Maximo(a, inf, medio);
+        int maxDer = Maximo (a, medio + 1, sup);        
+        return Math.min(maxIzq, maxDer);  
+        
+    }
     
     public static void main(String[] args) {
      
         int[]enteros ={1,4,8,3,5,1,7,9};
+        int[]enteros2 = new int [enteros.length ];
         
-        
-        System.out.println("Primer numero mayor = " + Maximo(enteros));        
-        System.out.println("Numero elevados a la 2 = " + Arrays.toString(multiplicar(enteros)));
+ 
+        for ( int i = 0; i < enteros.length ; i++){
+                int mul = enteros[i] * enteros[i];
+                enteros2[i] = mul ;   
+                       
+            } 
+                
+        System.out.println("Primer numero mayor = " + Maximo(enteros)); 
+        System.out.println("Segundo numero mayor = " + Minimo(enteros));  
+        System.out.println("Numero elevados a la 2 = " + Arrays.toString(enteros2));
+        System.out.println("Primer numero elevado a la 2 = " + Maximo(enteros2));
+        System.out.println("Segundo numero mayor elevado a la 2 = " + Minimo(enteros2));
 
     }
 
